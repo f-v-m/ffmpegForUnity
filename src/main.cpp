9a2128a3546786574b5796ad31c0ce03aed14fe9
f-v-m <f_v_m@MacBook-Pro-fedorkovych.local>
2015-04-22 18:00:13 +0300

    AVFrame to jpeg

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 
+
 extern "C"
 {
 #include <libavcodec/avcodec.h>
@@ -13,153 +14,173 @@ extern "C"
 #include <libavformat/avio.h>
 #include <libswscale/swscale.h>
 }
+using namespace std;
+
+int WriteJPEG (AVCodecContext *pCodecCtx, AVFrame *pFrame, int FrameNo){
+    AVCodecContext         *pOCodecCtx;
+    AVCodec                *pOCodec;
+    pOCodec = avcodec_find_decoder(CODEC_ID_H264);
+    uint8_t                *Buffer;
+    int                     BufSiz;
+    int                     BufSizActual;
+
+    FILE                   *JPEGFile;
+    char                    JPEGFName[256];
+    AVDictionary **options;
+    cout<<FrameNo<<endl;
+    BufSiz = avpicture_get_size(PIX_FMT_YUVJ420P,pCodecCtx->width,pCodecCtx->height );
+    
+    Buffer = (uint8_t *)malloc ( BufSiz );
+    if ( Buffer == NULL )
+        return ( 0 );
+    memset ( Buffer, 0, BufSiz );
+    
+    pOCodecCtx = avcodec_alloc_context3(pOCodec);
+    if ( !pOCodecCtx ) {
+        free ( Buffer );
+        return ( 0 );
+    }
+    cout<<FrameNo<<endl;
+    pOCodecCtx->bit_rate      = pCodecCtx->bit_rate;
+    pOCodecCtx->width         = pCodecCtx->width;
+    pOCodecCtx->height        = pCodecCtx->height;
+    pOCodecCtx->pix_fmt       = PIX_FMT_YUVJ420P;
+    pOCodecCtx->codec_id      = CODEC_ID_MJPEG;//CODEC_ID_H264;
+    pOCodecCtx->codec_type    = AVMEDIA_TYPE_VIDEO;
+    pOCodecCtx->time_base.num = pCodecCtx->time_base.num;
+    pOCodecCtx->time_base.den = pCodecCtx->time_base.den;
+    
+    pOCodec = avcodec_find_encoder ( pOCodecCtx->codec_id );
+    if ( !pOCodec ) {
+        free ( Buffer );
+        return ( 0 );
+    }
+    if ( avcodec_open2(pOCodecCtx, pOCodec, options) < 0 ) {
+        free ( Buffer );
+        return ( 0 );
+    }
+    
+    pOCodecCtx->mb_lmin        = pOCodecCtx->lmin =
+    pOCodecCtx->qmin * FF_QP2LAMBDA;
+    pOCodecCtx->mb_lmax        = pOCodecCtx->lmax =
+    pOCodecCtx->qmax * FF_QP2LAMBDA;
+    pOCodecCtx->flags          = CODEC_FLAG_QSCALE;
+    pOCodecCtx->global_quality = pOCodecCtx->qmin * FF_QP2LAMBDA;
+    
+    pFrame->pts     = 1;
+    pFrame->quality = pOCodecCtx->global_quality;
+    BufSizActual = avcodec_encode_video(
+                                        pOCodecCtx,Buffer,BufSiz,pFrame );
+    
+    sprintf ( JPEGFName, "%06d.jpg", FrameNo );
+    JPEGFile = fopen ( JPEGFName, "wb" );
+    fwrite ( Buffer, 1, BufSizActual, JPEGFile );
+    fclose ( JPEGFile );
+    cout<<JPEGFName<<endl;
+    avcodec_close ( pOCodecCtx );
+    free ( Buffer );
+    return ( BufSizActual );
+}
 
 
 
-int main(int argc, char** argv) {
+
+
+int main()
+{
+
+    
     using namespace std;
-
-    SwsContext *img_convert_ctx;
-    AVFormatContext* context = avformat_alloc_context();
-    AVCodec *codec = NULL;
+    int got_output;
+    //init all codecs
+    //avcodec_register_all();
     av_register_all();
     avformat_network_init();
     
+    //
+    
+    SwsContext *img_convert_ctx;
+    AVFormatContext* avf_context = avformat_alloc_context();
+    AVCodec *codec = NULL;
     codec = avcodec_find_decoder(AV_CODEC_ID_H264);
-    if (!codec) exit(1);
-
-    AVCodecContext* ccontext = avcodec_alloc_context3(codec);
+    if (!codec){
+        cout<<"Could not find codec"<<endl;
+        exit(1);
+    }
+    AVCodecContext* avc_context = avcodec_alloc_context3(codec);
     int video_stream_index;
     
     
-
-    
-    //open rtsp
-    if(avformat_open_input(&context, "http://127.0.0.1:1234",NULL,NULL) != 0){
+    //open stream:
+    if(avformat_open_input(&avf_context, "http://127.0.0.1:1234",NULL,NULL) != 0){
         return EXIT_FAILURE;
     }
     
-    if(avformat_find_stream_info(context,NULL) < 0){
+    if(avformat_find_stream_info(avf_context,NULL) < 0){
         return EXIT_FAILURE;
     }
     
     //search video stream
-    for(int i =0;i<context->nb_streams;i++){
-        if(context->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+    for(int i =0;i<avf_context->nb_streams;i++){
+        if(avf_context->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
             video_stream_index = i;
     }
     
     AVPacket packet;
     av_init_packet(&packet);
     
-    //open output file
-    //AVOutputFormat* fmt = av_guess_format(NULL,"test2.mp4",NULL);
     AVFormatContext* oc = avformat_alloc_context();
-    //oc->oformat = fmt;
-    //avio_open2(&oc->pb, "test.mp4", AVIO_FLAG_WRITE,NULL,NULL);
     
     AVStream* stream=NULL;
-    int cnt = 0;
-    //start reading packets from stream and write them to file
-    av_read_play(context);//play RTSP
-    
-    
-    
-    avcodec_get_context_defaults3(ccontext, codec);
-    avcodec_copy_context(ccontext,context->streams[video_stream_index]->codec);
-    std::ofstream myfile;
+    int count = 0;
+
+    //play stream
+    av_read_play(avf_context);
+
+    avcodec_get_context_defaults3(avc_context, codec);
+    avcodec_copy_context(avc_context, avf_context->streams[video_stream_index]->codec);
+
     AVDictionary **options;
-    //if (avcodec_open(ccontext, codec) < 0) exit(1);
+    if(avcodec_open2(avc_context, codec, options)<0) exit(0);
     
-    if (avcodec_open2(ccontext, codec, options)<0) exit(1);
+    img_convert_ctx = sws_getContext(avc_context->width, avc_context->height, avc_context->pix_fmt, avc_context->width, avc_context->height, PIX_FMT_YUVJ420P, SWS_BICUBIC, NULL, NULL, NULL);
     
-    img_convert_ctx = sws_getContext(ccontext->width, ccontext->height, ccontext->pix_fmt, ccontext->width, ccontext->height,
-                                     PIX_FMT_YUVJ420P, SWS_BICUBIC, NULL, NULL, NULL);
-    
-    int size = avpicture_get_size(PIX_FMT_YUV420P, ccontext->width, ccontext->height);
-    uint8_t* picture_buf = (uint8_t*)(av_malloc(size));
+    int pic_size = avpicture_get_size(PIX_FMT_YUVJ420P, avc_context->width, avc_context->height);
+    uint8_t* pic_buffer = (uint8_t*)(av_malloc(pic_size));
+    cout<<pic_size<<"!!"<<pic_buffer<<endl;
     AVFrame* pic = avcodec_alloc_frame();
-    AVFrame* picrgb = avcodec_alloc_frame();
-    int size2 = avpicture_get_size(PIX_FMT_RGB24, ccontext->width, ccontext->height);
-    uint8_t* picture_buf2 = (uint8_t*)(av_malloc(size2));
-    avpicture_fill((AVPicture *) pic, picture_buf, PIX_FMT_YUV420P, ccontext->width, ccontext->height);
-    avpicture_fill((AVPicture *) picrgb, picture_buf2, PIX_FMT_RGB24, ccontext->width, ccontext->height);
     
-    int arr_size = ccontext->height*ccontext->width*3*4 + 512;
-    unsigned char *arr = new unsigned char[arr_size];
-    char t[20];
+    avpicture_fill((AVPicture *) pic, pic_buffer, PIX_FMT_YUV420P, avc_context->width, avc_context->height);
     
-    while(av_read_frame(context,&packet)>=0)
-    {//read 100 frames
-        
-        std::cout << "1 Frame: " << cnt << std::endl;
-        if(packet.stream_index == video_stream_index){//packet is video
-            std::cout << "2 Is Video" << std::endl;
-            if(stream == NULL)
-            {//create stream in file
-                std::cout << "3 create stream" << std::endl;
-                stream = avformat_new_stream(oc,context->streams[video_stream_index]->codec->codec);
-                avcodec_copy_context(stream->codec,context->streams[video_stream_index]->codec);
-                stream->sample_aspect_ratio = context->streams[video_stream_index]->codec->sample_aspect_ratio;
-            }
+    int dest_size = avc_context->width *3 *avc_context->height;
+
+
+    while(av_read_frame(avf_context,&packet)>=0)
+    {
+        if(packet.stream_index==video_stream_index){
+            //Decode video frame:
             int check = 0;
-            packet.stream_index = stream->id;
-            std::cout << "4 decoding" << std::endl;
-            int result = avcodec_decode_video2(ccontext, pic, &check, &packet);
-            std::cout << "Bytes decoded " << result << " check " << check << std::endl;
+            int result = avcodec_decode_video2(avc_context, pic, &check, &packet);
+            cout<<result<<" decoded bytes"<<endl;
             
-            
-            unsigned char * ptr = arr;
-            
-            
-            memset(t,'0',20);
-            sprintf(t,"P3 %d %d 255\n", ccontext->width, ccontext->height);
-            memcpy(ptr, t, strlen(t));
-            ptr += strlen(t);
-            
-            /*
-            for(int y = 0; y < ccontext->height; y++)
-            {
-                for(int x = 0; x < ccontext->width * 3; x++){
-                    
-                    memset(t,'0',20);
-                    sprintf(t,"%d ", (int)(picrgb->data[0] + y * picrgb->linesize[0])[x]);
-                    //                    myfile << (int)(picrgb->data[0] + y * picrgb->linesize[0])[x] << " ";
-                    memcpy(ptr, t, strlen(t));
-                    ptr += strlen(t);
-                    
-                }
+            if (check){
+                WriteJPEG(avc_context, pic, count);
                 
+                //Write to file:
+                
+                /* Write JPEG to file */
+                
+            
             }
-            */
-            //get jpeg data
-            char buf[32] = {0};
-            snprintf(buf,30,"fram%d.jpeg",cnt);
-            FILE *fdJPEG = fopen(buf, "wb");
-            int bRet = fwrite(&size, sizeof(uint8_t), result, fdJPEG);
-            fclose(fdJPEG);
-            
-            //avpicture_fill((AVPicture *)pic, ptr, PIX_FMT_RGB24, 1280, 720);
-            
-            
-            
-            
-            
-            cnt++;
-            if (cnt == 800) exit(1);
         }
-        av_free_packet(&packet);
-        av_init_packet(&packet);
+        count++;
     }
     av_free(pic);
-    av_free(picrgb);
-    av_free(picture_buf);
-    av_free(picture_buf2);
+    av_free(pic_buffer);
     
-    av_read_pause(context);
-    avio_close(oc->pb);
-    avformat_free_context(oc);
     
-    return (EXIT_SUCCESS);
+    
+    //free memory
+    av_read_pause(avf_context);
+    avformat_free_context(avf_context);
 }
-
